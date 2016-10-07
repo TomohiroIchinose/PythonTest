22735e2c00837cc8ed695d578457ed391635755e
Tomohiro <admin@example.com>
2016-10-07 17:26:30 +0900

    first

---
+++ code1.java
@@ -0,0 +1,18 @@
+public class Hajimete {
+    public static void main(String[] args){
+	// TODO #1
+	System.out.println("Good morning, world.");
+
+	// TODO #2
+        System.out.println("Hello, world.");
+
+	// TODO #3
+        System.out.println("Good afternoon, world.");
+
+	// TODO #4
+        System.out.println("Good evening, world.");
+
+	// TODO #5
+        System.out.println("Good night, world.");
+    }
+}
---
+++ code2.java
@@ -0,0 +1,18 @@
+public class First {
+    public static void main(String[] args){
+	// TODO #6
+	System.out.println("Good morning, NAIST.");
+
+	// TODO #7
+        System.out.println("Hello, NAIST.");
+
+	// TODO #8
+        System.out.println("Good afternoon, NAIST.");
+
+	// TODO #9
+        System.out.println("Good evening, NAIST.");
+
+	// TODO #10
+        System.out.println("Good night, NAIST.");
+    }
+}
